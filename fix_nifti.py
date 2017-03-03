a9cc095477f1f759b9c2dc246dd673830f9c7aa4
Carlos Correa <carloscorrea137@gmail.com>
2017-03-03 15:27:17 -0800

    include more robust routine for fixing converted mdeft

--- fix_nifti.py
+++ fix_nifti.py
@@ -12,6 +12,7 @@ warp script. fix_nifti.py sets these values to 1 to avoid this issue.
 import nibabel as nib
 import sys
 import os
+import subprocess
 
 if len(sys.argv) != 3:
     print '''
@@ -34,16 +35,30 @@ if os.path.exists(output_file):
 
 image = nib.load(input_file)
 
+incorrect_headers = False
 if image.header['descrip'] == 'VISTASOFT':
-    already_aligned = False
     for key in ['qform_code', 'sform_code']:
         if image.header[key] == 2:
-            already_aligned = True
+            incorrect_headers = True
             image.header[key] = 1
 
-    if already_aligned:
+    if incorrect_headers:
         print (
             'warning: native image input headers suggest it might be '
-            'mistakenly marked as aligned by vistasoft')
+            'mistakenly marked as aligned by vistasoft. going to fix all headers')
+
+        # XXX assert that sform is all 1s and != qform
+        # While the pixdim and qform of these images are ok, the sform is incorrect.
+        # We will overwrite the sform with the qform.
+        aff = image.header.get_qform()
+        # force affine and sform to qform so we can reset. Only setting
+        # one or the other might not work.
+        # see more details here: https://github.com/nipy/nibabel/pull/90
+        image.affine[:] = aff
+        image.header.set_sform(aff)
 
 nib.save(image, output_file)
+
+if incorrect_headers:
+    # XXX assert that image is currently LPI?
+    subprocess.check_call(['3drefit', '-orient', 'LAI', output_file])
